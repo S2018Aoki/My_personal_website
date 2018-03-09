9998a98dd1fe6623fa445529c501b248d2485ea0
ShimaAoki <island_aoki@hotmail.com>
2018-03-09 11:41:12 -0500

    Enqueue google font Mrs Saint Delafield

--- wp-content/themes/child of twentyseventeen/functions.php
+++ wp-content/themes/child of twentyseventeen/functions.php
@@ -1,20 +1,20 @@
 <?php
 
 function wpb_add_google_fonts() {
-wp_enqueue_style ( 'wpb-google-fonts', 'https://fonts.googleapis.com/css?family=Mrs+Saint+Delafield', false );
-}
+wp_enqueue_style ( 'wpb-google-fonts', 'https://fonts.googleapis.com/css?family=Mrs+Saint+Delafield', 'https://fonts.googleapis.com/css?family=Cormorant+Garamond', false );
 
-function my_theme_enqueue_styles() {
+function child_of_twentyseventeen_enqueue_styles() {
 
-    $parent_style = 'twentyseventeen-style'; // This is 'twentyseventeen-style' for the Twenty Seventeen theme.
+    $parent_style = 'parent-style'; // This is 'twentyseventeen-style' for the Twenty Seventeen theme.
 
-    wp_enqueue_style( 'parent_style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( '$parent_style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css',
-        array( 'parent_style' ),
+        array( '$parent_style' ),
         wp_get_theme()->get('Version')
     );
 }
 
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
-?>
+add_action( 'wp_enqueue_scripts', 'child_of_twentyseventeen_enqueue_styles' );
 
+}
+?>
