91584a846694855a4f25cbc3bfabe6886494d322
ShimaAoki <island_aoki@hotmail.com>
2018-03-09 13:00:09 -0500

    test to delete title

--- wp-content/themes/child of twentyseventeen/notitle-page.php
+++ wp-content/themes/child of twentyseventeen/notitle-page.php
@@ -1,6 +1,13 @@
 <?php
 /**
- * The template modified for displaying page content on a new default front page called front-page.php
+ * Template Name: notitle-page.php 
+ *
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
@@ -10,21 +17,27 @@
  * @version 1.0
  */
 
-?>
+get_header(); ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php //the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-		<?php twentyseventeen_edit_link( get_the_ID() ); ?>
-	</header><!-- .entry-header -->
-	<div class="entry-content">
-		<?php
-			the_content();
+<div class="wrap">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
 
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'twentyseventeen' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
-</article><!-- #post-## -->
+			<?php
+			while ( have_posts() ) : the_post();
+
+				get_template_part( 'template-parts/page/content-notitle', 'notitle-page' );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+			endwhile; // End of the loop.
+			?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+</div><!-- .wrap -->
+
+<?php get_footer();
